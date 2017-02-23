fad39bd34bd06eaee3d004a1fd42f6d8cc28b56e
phizeroth <phi.zeroth@gmail.com>
2017-02-22 19:46:22 -0500

    misc

--- default/main.js
+++ default/main.js
@@ -14,16 +14,16 @@ module.exports.loop = function() {
     var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
     var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
 
-    if (harvesters.length < 3 && Game.spawns.SpawnAlpha.room.energyAvailable > 350) {
-        let newName = Game.spawns['SpawnAlpha'].createCreep([WORK, WORK, CARRY, CARRY, MOVE], undefined, {role: 'harvester'});
+    if (harvesters.length < 3 && Game.spawns.SpawnAlpha.room.energyAvailable >= 300) {
+        let newName = Game.spawns['SpawnAlpha'].createCreep([WORK, WORK, CARRY, MOVE], undefined, {role: 'harvester'});
         console.log('Spawning new harvester: ' + newName);
     }
-    if (upgraders.length < 2 && Game.spawns.SpawnAlpha.room.energyAvailable > 300) {
+    else if (upgraders.length < 2 && Game.spawns.SpawnAlpha.room.energyAvailable >= 300) {
         let newName = Game.spawns['SpawnAlpha'].createCreep([WORK, WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new upgrader: ' + newName);
     }
 
-    if (builders.length < 3 && Game.spawns.SpawnAlpha.room.energyAvailable > 400) {
+    else if (builders.length < 2 && Game.spawns.SpawnAlpha.room.energyAvailable >= 400) {
         let newName = Game.spawns['SpawnAlpha'].createCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE], undefined, {role: 'builder'});
         console.log('Spawning new builder: ' + newName);
     }
